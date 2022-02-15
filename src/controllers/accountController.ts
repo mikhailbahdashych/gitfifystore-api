0cb28bac5d75309137aa0fc7610d402dd13459e3
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-15 17:46:38 +0100

    register service

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
 const accountService = require('../services/accountService')
+const jwtService = require('../services/jwtService')
+const cryptoService = require('../services/cryptoService')
 
 export const register = async (req: Request, res: Response) => {
   try {
     let { email, password } = req.body
     const user = await accountService.getUserByEmail(email)
 
+    password = cryptoService.encrypt(password, process.env.CRYPTO_KEY)
     if (!user) {
       await accountService.createUser({
-        email, password: '123'
+        email, password
       })
     }
   } catch (e) {
