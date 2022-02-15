17273fd9fb1e4d29437cfa86b384dbcfd3806500
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-15 17:04:45 +0100

    migration

--- src/knex/migrations/20220214210530_users.js
+++ src/knex/migrations/20220214210530_users.js
@@ -1,6 +1,14 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', table => {
-    table.uuid('id')
+    table.uuid('id').notNullable().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.text('email').notNullable()
+    table.text('password').notNullable()
+
+    table.timestamp("created_at").defaultTo(knex.fn.now())
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+
   })
 };
 
