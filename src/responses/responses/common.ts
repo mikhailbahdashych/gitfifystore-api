7d688bf52549f798ab9579b8840f09f8d38c7601
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-23 20:00:43 +0100

    common response class

---
+++ src/responses/responses/common.ts
@@ -0,0 +1,31 @@
+import { ResponsePayload } from "../../interfaces/objects/ResponsePayload";
+
+export class CommonResponse {
+
+  public unauthorized(data: ResponsePayload) {
+    const { res } = data
+
+    return res.status(401).json({
+      status: 401,
+      message: 'unauthorized'
+    })
+  }
+
+  public somethingWentWrong(data: ResponsePayload) {
+    const { res } = data;
+
+    return res.status(500).json({
+      status: 500,
+      message: 'something-went-wrong'
+    })
+  }
+
+  public accessForbidden(data: ResponsePayload) {
+    const { res } = data;
+
+    return res.status(403).json({
+      status: 403,
+      message: 'access-forbidden'
+    })
+  }
+}
