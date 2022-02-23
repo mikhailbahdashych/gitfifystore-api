5134d851cd7a13ace7a25b4fdbb3b1d54caec0c4
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-23 21:17:39 +0100

    comms

--- src/services/cryptoService.ts
+++ src/services/cryptoService.ts
@@ -17,21 +17,3 @@ export const hashPassword = (password: string, key: string) => {
   const sha256Hasher = crypto.createHmac("sha256", key);
   return sha256Hasher.update(password).digest("hex");
 }
-
-// module.exports = {
-//   encrypt: (text: string, key: string, iv: string) => {
-//     let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-//     let encrypted = cipher.update(text, 'utf8', 'base64');
-//     encrypted += cipher.final('base64');
-//     return encrypted;
-//   },
-//   decrypt: (text: string, key: string, iv: string) => {
-//     let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-//     let decrypted = decipher.update(text, 'base64', 'utf8');
-//     return (decrypted + decipher.final('utf8'));
-//   },
-//   hashPassword: (password: string, key: string) => {
-//     const sha256Hasher = crypto.createHmac("sha256", key);
-//     return sha256Hasher.update(password).digest("hex");
-//   }
-// }
