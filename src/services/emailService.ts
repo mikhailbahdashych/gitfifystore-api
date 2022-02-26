185a313cf0de03861067da2499ad8ac0d593c5df
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-26 22:07:42 +0100

    email service

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,6 +1,26 @@
 const nodemailer = require('nodemailer')
 
 export const sendEmail = async (message: string) => {
-  console.log('messagemessagemessage', message)
-  return message
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: '',
+      pass: ''
+    }
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <>',
+    to: "",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>",
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
 }
