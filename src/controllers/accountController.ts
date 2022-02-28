fd42b6e1f470a13af4016ba411df3f2a4ad05ff2
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-02-28 18:49:10 +0100

    small fixes

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -9,8 +9,6 @@ import * as cryptoService from '../services/cryptoService';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-// TODO res without return?
-
 import { CommonResponse } from "../responses/response";
 
 const logger = loggerConfig({ label: 'account-controller', path: 'account' })
--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -7,9 +7,7 @@ export default (fields: any) => {
       await require(`./validators/${field}`)(req);
     }
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     next();
   }
 }
