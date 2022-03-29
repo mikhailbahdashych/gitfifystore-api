0a053cbff051745ff62c8431cb6989374845b630
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-03-29 19:52:34 +0200

    hide phone

--- src/common/hiders.ts
+++ src/common/hiders.ts
@@ -3,3 +3,8 @@ export const hideEmail = (email: string) => {
   return email.split('@')[0].slice(0, 2) + '**' + '@**.'
     + email.split('.')[email.split('.').length - 1]
 }
+
+export const hidePhone = (phone: string) => {
+  if (!phone) return
+  return phone.slice(0, 3) + '******'
+}
